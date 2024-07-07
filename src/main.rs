559a6361a058ea521414b2dd1decc5144f4f549e
29 <791603901@qq.com>
2024-07-07 18:45:11 +0800

    添加tracing日志功能

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use pushover_rs::{send_pushover_request, PushoverSound};
 use std::{env, time::Duration};
 use tokio::time::sleep;
+use tracing_subscriber::fmt::{format::FmtSpan, time::ChronoLocal};
 
 #[derive(Parser, Debug)]
 enum Commands {
@@ -32,16 +33,43 @@ impl Pushover {
     }
 }
 
+#[cfg(debug_assertions)]
+pub fn install_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_max_level(tracing::Level::DEBUG)
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
+#[cfg(not(debug_assertions))]
+pub fn install_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    #[cfg(windows)]
+    nu_ansi_term::enable_ansi_support().ok();
+    install_tracing();
+
     let pushover = Pushover {
         user_key: env::var("PUSHOVER_USER_KEY")?,
         app_token: env::var("PUSHOVER_APP_TOKEN")?,
     };
 
     match Commands::parse() {
-        Commands::Start => pushover.send("Workflow开始执行！", PushoverSound::BIKE),
+        Commands::Start => {
+            tracing::info!("workflow-bin start");
+            pushover.send("Workflow开始执行！", PushoverSound::BIKE)
+        }
         Commands::Run => {
+            tracing::info!("workflow-bin run");
             sleep(Duration::from_secs(10)).await;
 
             if true {
